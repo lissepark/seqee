c20541274a1ef3246a7b9ed5255b7c8b872fae4b
Sergii Varenyk <seva0716@netcracker.com>
2018-01-16 07:02:44 +0200

    adding category test 25

--- src/main/java/database/DBConnection.java
+++ src/main/java/database/DBConnection.java
@@ -162,7 +162,6 @@ public class DBConnection {
                 offer.setId(rs.getLong("offering_id"));
                 offer.setOfferName(rs.getString("offering_name"));
                 offer.setOfferDescription(rs.getString("offering_description"));
-                offer.setOfferImageName(rs.getString("offer_image_name"));
                 offer.setOfferMainImage((com.mysql.jdbc.Blob) rs.getBlob("image_nat"));
                 result.add(offer);
             }
