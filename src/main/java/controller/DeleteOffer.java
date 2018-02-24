1470774c4ba803608578a3d7c943d49d7fc9865c
Sergii Varenyk <seva0716@netcracker.com>
2018-02-24 13:53:57 +0200

    edit offer 31

--- src/main/java/controller/DeleteOffer.java
+++ src/main/java/controller/DeleteOffer.java
@@ -21,7 +21,7 @@ public class DeleteOffer extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String cat_id_str = request.getParameter("category_id");
         int catid = Integer.parseInt(cat_id_str);
-        //String offer_id_str = request.getParameter("offer_id");
+        String offer_id_str = request.getParameter("offer_id");
         //ApplicationContext actx = new ClassPathXmlApplicationContext("beans.xml");
         //OfferDAO offerDAO = (OfferDAO) actx.getBean("daoImpl");
         //try {
@@ -29,8 +29,9 @@ public class DeleteOffer extends HttpServlet {
         //} catch (SQLException e) {
         //    e.printStackTrace();
         //}
+        request.setAttribute("category_id", cat_id_str);
         RequestDispatcher requestDispatcher;
-        requestDispatcher = request.getRequestDispatcher("/WEB-INF/jsp/offers?category_id="+catid);
+        requestDispatcher = request.getRequestDispatcher("/WEB-INF/jsp/offers");
         requestDispatcher.forward(request, response);
     }
 
